266d6a75b21ff1adb8bfbb88c2c4f49790cd30a7
Craig Treasure <crtreasu@microsoft.com>
2020-10-20 14:07:38 -0700

    Create the HelloWorld sample

---
+++ sdk/api-learn/Azure.Learn.AppConfig/src/ConfigurationSetting.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Learn.AppConfig
+{
+    /// <summary>
+    /// The <see cref="ConfigurationSetting"/>.
+    /// </summary>
+    [CodeGenModel("KeyValue")]
+    public partial class ConfigurationSetting
+    {
+        /// <summary>
+        /// The entity-tag of the key-value.
+        /// </summary>
+        [CodeGenMember("Etag")]
+        public ETag ETag { get; }
+    }
+}
