cda5650ab666752a5af6f35a113a21cd8c875de7
Jacob Stenzel <jastenze@microsoft.com>
2021-01-28 13:51:41 -0800

    temp

---
+++ sdk/mixedreality/Azure.MixedReality.ObjectAnchors/src/ObjectAnchorsAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Core;
+
+namespace Azure.MixedReality.ObjectAnchors
+{
+    public class ObjectAnchorsAccount
+    {
+        /// <summary>
+        /// Gets the Azure Spatial Anchors account domain.
+        /// </summary>
+        public string AccountDomain { get; }
+
+        /// <summary>
+        /// Gets the Azure Spatial Anchors account identifier.
+        /// </summary>
+        public string AccountId { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectAnchorsAccount"/> class.
+        /// </summary>
+        /// <param name="accountId">The Azure Object Anchors account identifier.</param>
+        /// <param name="accountDomain">The Azure Object Anchors account domain.</param>
+        public ObjectAnchorsAccount(string accountId, string accountDomain)
+        {
+            Argument.AssertNotNullOrWhiteSpace(accountId, nameof(accountId));
+            Argument.AssertNotNullOrWhiteSpace(accountDomain, nameof(accountDomain));
+
+            AccountId = accountId;
+            AccountDomain = accountDomain;
+        }
+    }
+}
